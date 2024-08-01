67f8497032e43ef08a0d288b0f8891227d1f4f69
Andrii <126251484+Andrew3552@users.noreply.github.com>
2024-08-01 21:34:03 +0300

    add test text

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-import { useEffect } from 'react';
-const tg = window.Telegram.WebApp
-
-
+// import { useEffect } from 'react';
+// const tg = window.Telegram.WebApp
+import "./App.css";
 
 function App() {
+  // useEffect(() => {
+  //   tg.ready();
+  // }, [])
 
-  useEffect(() => {
-    tg.ready();
-  }, [])
-
-  const onClose = () => {
-    tg.close
-  }
+  // const onClose = () => {
+  //   tg.close
+  // }
   return (
     <div className="App">
-    <button onClick={onClose}>Close</button>
+      <p className="text">привет підстрежися</p>
     </div>
   );
 }
 
 export default App;
+
